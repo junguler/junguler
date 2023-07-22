9b1856fb94c0bd708089bc66e59c11c918928158
U-DESKTOP-3VIS1T9\junguler <junguler@yahoo.com>
2023-07-22 07:18:46 +0330

    +

--- color.sh
+++ color.sh
@@ -4,7 +4,7 @@ for i in {A..Z} {0..9}
 do echo $i
 printf "%02x%02x%02x" $((RANDOM%256)) $((RANDOM%256)) $((RANDOM%256)) > c1.txt
 printf "%02x%02x%02x" $((RANDOM%256)) $((RANDOM%256)) $((RANDOM%256)) > c2.txt
-magick convert -gravity center -trim -background \#$(cat c1.txt) -fill \#$(cat c2.txt) -size 100x100 caption:$i -extent 124x124 $i.jpg
+magick convert -gravity center -trim -background \#$(cat c1.txt) -fill \#$(cat c2.txt) -size 50x50 caption:$i -extent 50x50 $i.jpg
 done
 
 rm c1.txt c2.txt
