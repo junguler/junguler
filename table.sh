1cda690df918e9c4be4a378da2b441041980d2ea
hossein s. borhani <junguler@yahoo.com>
2023-09-17 07:50:45 +0330

    convert table to be in 16x6 - wh

--- table.sh
+++ table.sh
@@ -4,10 +4,10 @@ items=($(cat <(ls *.gif) <(echo error) <(echo empty) <(echo nothing) | shuf))
 
 echo "<table>" > readme.md
 
-for ((i=0; i<8; i++)); do
+for ((i=0; i<6; i++)); do
   echo "<tr>" >> readme.md
-  for ((j=0; j<12; j++)); do
-    index=$((i*12+j))
+  for ((j=0; j<16; j++)); do
+    index=$((i*16+j))
     echo "<td><img src=\"${items[$index]}\"></td>" >> readme.md
   done
   echo "</tr>" >> readme.md
